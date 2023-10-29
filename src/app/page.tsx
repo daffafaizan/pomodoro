d5caec4d039386899d436accf1e995ae9779e3bd
Daffa <daffafaizan@Daffas-MacBook-Pro.local>
2023-10-29 22:19:07 +0700

    Added break

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [isRunning, setIsRunning] = useState(false);
-  const [displayMessage, setDisplayMessage] = useState(false);
+  const [breakMessage, setBreakMessage] = useState(false);
   const [seconds, setSeconds] = useState(5);
   const [minutes, setMinutes] = useState(0);
   const secondsFormat = seconds < 10 ? `0${seconds}` : `${seconds}`;
@@ -20,12 +20,13 @@ export default function Home() {
             setSeconds(59);
             setMinutes(minutes - 1);
           } else {
-            let minutes = displayMessage ? 24 : 4;
-            let seconds = 59;
+            let minutes = breakMessage ? 24 : 5;
+            let seconds = 0;
 
             setSeconds(seconds);
             setMinutes(minutes);
-            setDisplayMessage(!displayMessage);
+            setIsRunning(false);
+            setBreakMessage(!breakMessage);
           }
         } else {
           setSeconds(seconds - 1);
@@ -38,27 +39,54 @@ export default function Home() {
     return () => {
       clearInterval(interval);
     };
-  }, [isRunning, seconds, minutes, displayMessage]);
+  }, [isRunning, seconds, minutes, breakMessage]);
 
   const handleStartStopClick = () => {
     setIsRunning(!isRunning);
-  }
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#98B4D4]">
-      <div className="flex flex-col items-center justify-center h-60 w-60 gap-2 rounded-full bg-slate-500 bg-opacity-30">
-        <div>
-          <span className="text-5xl text-white">
-            {minutesFormat}:{secondsFormat}
-          </span>
-        </div>
-        <div>
-          <button className="text-lg px-4 py-1 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform"
-          onClick={handleStartStopClick}>
-            {isRunning ? 'stop' : 'start'}
-          </button>
-        </div>
-      </div>
-    </main>
+    <>
+      {!breakMessage ? (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#98B4D4]">
+          <div className="flex flex-col items-center justify-center h-60 w-60 gap-2 rounded-full bg-slate-500 bg-opacity-30">
+            <div>
+              <span className="text-5xl text-white">
+                {minutesFormat}:{secondsFormat}
+              </span>
+            </div>
+            <div>
+              <button
+                className="text-lg px-4 py-1 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform"
+                onClick={handleStartStopClick}
+              >
+                {isRunning ? "stop" : "start"}
+              </button>
+            </div>
+          </div>
+        </main>
+      ) : (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-[#EF3340]">
+          <div className="flex flex-col items-center justify-center h-60 w-60 gap-2 rounded-full bg-gray-300 bg-opacity-20">
+            <div className="text-sm">
+              Time for a break
+            </div>
+            <div>
+              <span className="text-5xl text-white">
+                {minutesFormat}:{secondsFormat}
+              </span>
+            </div>
+            <div>
+              <button
+                className="text-lg px-4 py-1 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform"
+                onClick={handleStartStopClick}
+              >
+                {isRunning ? "stop" : "start"}
+              </button>
+            </div>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
