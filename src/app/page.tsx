84ee323becaee60093a7f9710b4b2c22b9db8561
Daffa <daffafaizan@Daffas-MacBook-Pro.local>
2023-10-29 22:56:33 +0700

    Added reset button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,30 +45,49 @@ export default function Home() {
     setIsRunning(!isRunning);
   };
 
+  const handleReset = () => {
+    setMinutes(35);
+    setSeconds(0);
+  };
+
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-center p-24"
       style={{
         background: breakMessage ? "#EF3340" : "#98B4D4",
-        transition: "background 0.5s ease", // Add a background color transition
+        transition: "background 0.5s ease",
       }}
     >
-      <div className="flex flex-col items-center justify-center h-60 w-60 gap-2 rounded-full bg-gray-300 bg-opacity-30">
-        <div>
+      <div className="flex flex-col items-center justify-center h-[350px] w-[300px] rounded-3xl bg-white shadow-xl drop-shadow-md">
+        <div
+          className="flex flex-col items-center justify-center h-52 w-52 gap-2 rounded-full bg-opacity-30"
+          style={{
+            background: breakMessage ? "#EF3340" : "#98B4D4",
+            transition: "background 0.5s ease",
+          }}
+        >
           <span className="text-5xl text-white">
             {minutesFormat}:{secondsFormat}
           </span>
         </div>
-        <div>
+        <div className="flex flex-row items-center justify-center w-full mt-7 gap-10">
           <button
-            className="text-lg px-4 py-1 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform"
+            className="text-lg px-4 py-1 h-10 w-50 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform shadow-md drop-shadow-md"
             onClick={handleStartStopClick}
             style={{
               color: breakMessage ? "#EF3340" : "#98B4D4",
-              transition: "color 0.5s ease",
             }}
           >
             {isRunning ? "stop" : "start"}
+          </button>
+          <button
+            className="text-lg px-4 py-1 h-10 w-50 rounded-md bg-white hover:scale-110 hover:duration-150 transition-transform shadow-md drop-shadow-md"
+            onClick={handleReset}
+            style={{
+              color: breakMessage ? "#EF3340" : "#98B4D4",
+            }}
+          >
+            reset
           </button>
         </div>
       </div>
